fa829226c1cc51d4bf9c3deb2a6342f0d88a097c
klaath <kbingol22@gmail.com>
2014-05-27 13:39:55 +0300

    lossy frequence count

--- states.py
+++ states.py
@@ -1,9 +1,11 @@
+from collections import namedtuple
 from sets import Set
 import os, time, sys
 import threading, Queue
 import os
 
 DEFAULT_TIMEOUT = 5
+FreqStruct = namedtuple("freqStruct", "state freq delta")
 
 class StateManager(threading.Thread):
 	def __init__(self, mem_limit, map_q, reduce_q, update_q ):
@@ -12,6 +14,8 @@ class StateManager(threading.Thread):
 		self.update_q = update_q
 		self.stoprequest = threading.Event()
 		self.in_mem_state = {}
+		self.currentBucket = 1	#bucket id
+		self.numOfItemProcessed = 0	#number of items processed so far in stream
 		self.processing_keys = Set()
 		self.map_q = map_q
 		self.reduce_q = reduce_q
@@ -47,7 +51,7 @@ class StateManager(threading.Thread):
 	def get_state(self, key):
 
 		if key in self.in_mem_state:
-			return self.in_mem_state[key]
+			return self.in_mem_state[key].state
 		else:
 			state_path = './states/%s'%(key)
 			if os.path.isfile(state_path):
@@ -60,14 +64,26 @@ class StateManager(threading.Thread):
 	def _update_state(self):
 		for i in xrange(self.update_q.qsize()):
 			(key, state) = self.update_q.get()
+			self.numOfItemProcessed += 1
 			if key in self.in_mem_state:
-				self.in_mem_state[key] = state
+				s = self.in_mem_state[key] 
+				s._replace(state = state, freq = s.freq + 1)
+				self.in_mem_state[key] = s
 			elif not key in self.in_mem_state and len(self.in_mem_state) < self.mem_limit:
-				self.in_mem_state[key] = state
-			else:
-				state_path = './states/%s'%(key)
-				f = open(state_path, 'w')
-				f.write('%s\n'%(str(state)))
-				f.close
+				self.in_mem_state[key] = FreqStruct(state = state, freq = 1, delta = self.currentBucket-1)
+
+			if self.numOfItemProcessed % self.mem_limit == 0:
+				tmpList = []
+				for k,v in self.in_mem_state.iteritems():
+					if v.freq + v.delta <= self.currentBucket:
+						tmpList.append(k)
+						state_path = './states/%s'%(k)
+						f = open(state_path, 'w')
+						f.write('%s\n'%(str(v.state)))
+						f.close
+				for k in tmpList:
+					del self.in_mem_state[k]		
+				self.currentBucket += 1			
+	
 			self.processing_keys.remove(key)
 					
--- words.rst
+++ words.rst
@@ -1,10 +1,10 @@
-ali ali ali
-veli veli veli veli
-murat murat
-kamil kamil
-beyza beyza beyza
+ali ali ali ali ali ali ali ali
+veli veli veli veli ali ali ali ali
+ali ali ali ali ali ali ali ali murat murat ali ali ali ali
+kamil kamil ali ali ali ali ali ali ali ali
+beyza beyza beyza ali ali ali ali ali ali ali ali
 hulya hulya hulya
-selim ayhan mevzu avea turkcell
+selim ayhan mevzu ali ali ali ali ali ali ali ali avea turkcell
 kahraman hakim soma
-toma katil 
+toma katil ali ali ali ali ali ali ali ali
 sanik aslan
